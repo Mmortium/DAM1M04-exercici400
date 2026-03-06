7cae60fa0b8d44a348c096bac4382074a916032e
Mmortium <molcliff@gmail.com>
2026-03-06 18:32:38 +0100

    funciona

--- public/script.js
+++ public/script.js
@@ -4,7 +4,6 @@ const COLS = 4;
 let moviments = 0;
 let jocAcabat = false;
 
-// Matriu 3x4 (11 peces + el 0 que és el buit)
 let tauler = [
     [1, 2, 3, 4],
     [5, 6, 7, 8],
@@ -12,16 +11,32 @@ let tauler = [
 ];
 
 const container = document.getElementById('puzzle-container');
+const txtMoviments = document.getElementById('comptador');
+const txtMissatge = document.getElementById('missatge');
+const btnReset = document.getElementById('btn-reset');
+
+function init() {
+    crearPecesDOM();
+    btnReset.onclick = iniciarJoc;
+    iniciarJoc();
+}
+
+function iniciarJoc() {
+    moviments = 0;
+    jocAcabat = false;
+    if(txtMoviments) txtMoviments.innerText = "0";
+    if(txtMissatge) txtMissatge.innerText = "";
+    barrejar();
+    dibuixar();
+}
 
 function crearPecesDOM() {
     container.innerHTML = "";
-    // Ara n'hi ha 11 peces (l'espai 12 és el 0)
     for (let i = 1; i <= 11; i++) {
         const div = document.createElement('div');
         div.id = `peca-${i}`;
         div.classList.add('peca');
         
-        // Càlcul de la posició original per a la imatge (3x4)
         const filaOriginal = Math.floor((i - 1) / COLS);
         const colOriginal = (i - 1) % COLS;
         
@@ -29,6 +44,27 @@ function crearPecesDOM() {
         
         div.onclick = () => clicarPeca(i);
         container.appendChild(div);
+    }
+}
+
+// AQUESTA FUNCIÓ ET FALTAVA:
+function clicarPeca(num) {
+    if (jocAcabat) return;
+
+    const pBuit = trobarPosicio(0);
+    const pPeca = trobarPosicio(num);
+
+    const df = Math.abs(pPeca.f - pBuit.f);
+    const dc = Math.abs(pPeca.c - pBuit.c);
+
+    if (df + dc === 1) {
+        tauler[pBuit.f][pBuit.c] = num;
+        tauler[pPeca.f][pPeca.c] = 0;
+        
+        moviments++;
+        if(txtMoviments) txtMoviments.innerText = moviments;
+        dibuixar();
+        comprovarVictoria();
     }
 }
 
@@ -54,18 +90,10 @@ function trobarPosicio(n) {
 }
 
 function barrejar() {
-    // Reset de la matriu a estat resolt per seguretat
-    tauler = [
-        [1, 2, 3, 4],
-        [5, 6, 7, 8],
-        [9, 10, 11, 0]
-    ];
-    
+    tauler = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 0]];
     for (let i = 0; i < 200; i++) {
         const buit = trobarPosicio(0);
         const adjacents = [];
-        
-        // Lògica de límits per a 3 files i 4 columnes
         if (buit.f > 0) adjacents.push({f: buit.f - 1, c: buit.c});
         if (buit.f < FILES - 1) adjacents.push({f: buit.f + 1, c: buit.c});
         if (buit.c > 0) adjacents.push({f: buit.f, c: buit.c - 1});
@@ -73,21 +101,19 @@ function barrejar() {
         
         const randomPos = adjacents[Math.floor(Math.random() * adjacents.length)];
         const valor = tauler[randomPos.f][randomPos.c];
-        
         tauler[buit.f][buit.c] = valor;
         tauler[randomPos.f][randomPos.c] = 0;
     }
 }
 
 function comprovarVictoria() {
-    // Generem l'string esperat per a un 3x4: "1,2,3,4,5,6,7,8,9,10,11,0"
     const resolt = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 0].join(",");
     const actual = tauler.flat().join(",");
-    
     if (actual === resolt && moviments > 0) {
         jocAcabat = true;
-        document.getElementById('missatge').innerText = `Resolt en ${moviments} moviments!`;
+        if(txtMissatge) txtMissatge.innerText = `Resolt en ${moviments} moviments!`;
     }
 }
 
+// CRIDEM LA FUNCIÓ PER COMENÇAR
 init();
